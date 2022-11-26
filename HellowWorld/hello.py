4dab42682a569a5f1d33e72d0427c97c2b3519dd
Tonu <tonuilves.est@gmail.com>
2022-11-26 07:28:52 -0800

    init nuitka test project

---
+++ HellowWorld/hello.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 20 10:45:24 2022
+
+@author: tonu
+"""
+
+def talk(message):
+    return "Talk " + message
+
+
+def main():
+    print(talk("Hello World"))
+
+
+if __name__ == "__main__":
+    main()
