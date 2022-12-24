b3228b7afa0ab6c045f9a3c2a6f54fda9ee165ef
Tonu <tonuilves.est@gmail.com>
2022-12-23 18:32:13 -0800

    remove webapp_checlist.odt from repo

--- flask_web_app_course/market/models.py
+++ flask_web_app_course/market/models.py
@@ -1,5 +1,14 @@
 from market import db
 
+class User(db.Model):
+    # 'db.relationship() creates relationla link between Item & User tables
+    id = db.Column(db.Integer(), primary_key=True)
+    username = db.Column(db.String(length=30), nullable=False, unique=True)
+    email = db.Column(db.String(length=50), nullable=False, unique=True)
+    password_hash = db.Column(db.String(length=60), nullable=False)
+    budget = db.Column(db.Integer(), nullable=False, default=1000)
+    items = db.relationship('Item', backref='owned_user', lazy=True)
+
 # create databse class
 class Item(db.Model):
     # 'id' is the convention in Flask apps.
@@ -10,6 +19,7 @@ class Item(db.Model):
     price = db.Column(db.Integer(), nullable=False)
     barcode = db.Column(db.String(length=12), nullable=False, unique=True)
     description = db.Column(db.String(length=1024), nullable=False, unique=True)
+    owner = db.Column(db.Integer(), db.ForeignKey('user.id'))
     
     # override the naming convention of the table item
     def __repr__(self):
